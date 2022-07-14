7fb31d13d930dd67e4578ae57fa45d384499af44
biotoscano <biotoscano@gmail.com>
2022-07-14 13:43:51 +0200

    new URL deploymen

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ function testGS() {
 
     console.log('BUTTON CLICKED');
 
-    const url = 'https://script.google.com/macros/s/AKfycbzQszoOcc7kAhfDiwG4JCn7qgJ3S3Pclz60SWxaqsV9sETj5HWgIUmYraGUGyTofzXYOg/exec';
+    const url = 'https://script.google.com/macros/s/AKfycbxIzgVLoQdiXryRw_pglluvYkEAAydrriFwxSf_DtrgqwAWbG-E09-dTy1TmwFecgsEAw/exec';
 
     fetch(url)
         .then(r => r.json())
