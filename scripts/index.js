d9ea13749dfaf9db8ffd93f94a1a42abd0afc367
biotoscano <biotoscano@gmail.com>
2022-07-14 13:59:50 +0200

    get and post work

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 
 document.getElementById('btnFetch').addEventListener('click', testGS)
+document.getElementById('btnPost').addEventListener('click', addGS)
 
 function testGS() {
 
@@ -17,3 +18,22 @@ function testGS() {
 
 }
 
+function addGS() {
+
+    const url = 'https://script.google.com/macros/s/AKfycbxIzgVLoQdiXryRw_pglluvYkEAAydrriFwxSf_DtrgqwAWbG-E09-dTy1TmwFecgsEAw/exec';
+
+    fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        // credentials: 'omit', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({name:"Carlos Toscano"}) // body data type must match "Content-Type" header
+    })
+
+}
