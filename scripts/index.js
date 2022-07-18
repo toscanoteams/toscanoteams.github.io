8b7fc52131abed612db2abf5d2cf1f7fb0c5384e
biotoscano <biotoscano@gmail.com>
2022-07-18 08:43:15 +0200

    form sends URL parameters correctly with Promise

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,49 @@
-
+document.getElementById('btnLogin').addEventListener('click', doLogin)
 document.getElementById('btnFetch').addEventListener('click', testGS)
 document.getElementById('btnPost').addEventListener('click', addGS)
 
-function testGS() {
+function doLogin(e) {
+
+    e.preventDefault();
 
     console.log('BUTTON CLICKED');
 
-    const url = 'https://script.google.com/macros/s/AKfycbxIzgVLoQdiXryRw_pglluvYkEAAydrriFwxSf_DtrgqwAWbG-E09-dTy1TmwFecgsEAw/exec';
+    let data = Object.fromEntries(new FormData(document.getElementById('userpass')).entries());
+    console.log(data);
+    const param0 = `${Object.keys(data)[0]}=${Object.values(data)[0]}`;
+    const param1 = `${Object.keys(data)[1]}=${Object.values(data)[1]}`;
+    const params = `?${param0}&${param1}`;
+
+    let url = 'https://script.google.com/macros/s/AKfycbyHHUluT1S5y3giWMEQ3aOqr5Fd_2eLggb2O3v8D2zlW2xWgxQM8HrM3JtMYJP2lBOeuQ/exec';
+    url += params;
+    console.log(params)
 
     fetch(url)
         .then(r => r.json())
         .then(r => {
             console.log(r);
-            console.log(r[0].status);
-            document.getElementById('fetchResponse').textContent = r[0].status;
+            document.getElementById('fetchResponse').textContent = r.status;
+        })
+
+    document.getElementById('userpass').reset();
+}
+
+function testGS() {
+
+    console.log('BUTTON CLICKED');
+
+    const url = 'https://script.google.com/macros/s/AKfycbyHHUluT1S5y3giWMEQ3aOqr5Fd_2eLggb2O3v8D2zlW2xWgxQM8HrM3JtMYJP2lBOeuQ/exec';
+
+    fetch(url)
+        .then(r => r.json())
+        .then(r => {
+            console.log(r);
+            document.getElementById('fetchResponse').textContent = r.status;
         })
 
 }
+
+const idb = window.indexedDB;
 
 function addGS() {
 
