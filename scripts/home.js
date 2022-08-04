aee531a0784724d14ab9eff7323a14c310a5185d
biotoscano <biotoscano@gmail.com>
2022-08-04 14:39:17 +0200

    header position fixed top

--- scripts/home.js
+++ scripts/home.js
@@ -11,28 +11,26 @@ async function afterLoad() {
     
     // console.log(tempID);
     // shirt
+    manageDB();
 
     fetch(url)
         .then(response => response.json())
-        .then(data => {manageDB(data.userDB); document.getElementById("beautified").innerHTML = JSON.stringify(data.userDB, undefined, 2);})
+        .then(data => {
+            
+            console.log(data.userDB);
+            // addData(data.userDB);
+            document.getElementById("beautified").innerHTML = JSON.stringify(data.userDB, undefined, 2);
+        })
         .catch(error => console.log(error));
 
-    // // await timeout(2000);
-    // const loadingBanner = Array.from(document.getElementsByClassName('loadingBanner'))[0];
-
-    // loadingBanner.style.top = '100%';
-    // loadingBanner.style.bottom = '-100%';
-    // loadingBanner.style.transition = '0.4s';
-    // // await timeout(1000);
-    // loadingBanner.style.display = 'none';
-    // loadingBanner.style.transitionTimingFunction = "ease-out";
+    
 }
 
 
 const idb = window.indexedDB;
-function manageDB(userDB) {
-    console.log('storing userinfo in indexedDB')
-    console.log(userDB)
+function manageDB() {
+    // console.log('storing userinfo in indexedDB')
+    // console.log(userDB)
 
     if(idb) {
         let db;
@@ -41,8 +39,11 @@ function manageDB(userDB) {
         request.onupgradeneeded = () => {
             db = request.result;
             console.log('CREATE', db);
-            const objectStore = db.createObjectStore('userPersonal');
-            const objectStore1 = db.createObjectStore('registroVacaciones');
+            const objectStore = db.createObjectStore('userData', {
+                autoIncrement: true
+            });
+            console.log(db);
+            // addData(userDB)
         };
 
         request.onsuccess = () => {
@@ -53,5 +54,27 @@ function manageDB(userDB) {
         request.onerror = (error) => {
             console.log('Error', error);
         };
+
+        // const addData = (data) => {
+        //     let db;
+        //     let request = idb.open('userDB', 1);
+        //     db = request.result;
+        //     console.log(db)
+        //     const transaction = db.transaction('userData','readwrite');
+        //     const objectStore = transaction.objectStore('userData');
+        //     request = objectStore.add(data);
+        // }
+
+        // addData(userDB)
     }
+}
+
+const addData = (data) => {
+    let db;
+    let request = idb.open('userDB', 1);
+    db = request.result;
+    console.log(db)
+    const transaction = db.transaction('userData','readwrite');
+    const objectStore = transaction.objectStore('userData');
+    request = objectStore.add(data);
 }
