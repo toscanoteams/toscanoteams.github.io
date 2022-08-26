291e3df43fd7d98592aac746267ab215a5cbade0
biotoscano <biotoscano@gmail.com>
2022-08-26 13:30:06 +0200

    disable indexeddb

--- scripts/home.js
+++ scripts/home.js
@@ -9,56 +9,87 @@ let readData;
 async function afterLoad() {
     console.log('function: manageDB()')
 
-    if (indexedDB) {
-        let db;
-        let request = indexedDB.open('userDB', 1);
+    // if (indexedDB) {
+    //     let db;
+    //     let request = indexedDB.open('userDB', 1);
 
-        request.onsuccess = () => {
-            db = request.result;
-            console.log('OPEN', db);
-            readData();
-        };
+    //     request.onsuccess = () => {
+    //         db = request.result;
+    //         console.log('OPEN', db);
+    //         // readData();
+    //     };
 
-        request.onupgradeneeded = () => {
-            db = request.result;
-            console.log('CREATE', db);
-            const objectStore = db.createObjectStore('userData', {
-                autoIncrement: true
-            });
-            console.log(db);
+    //     request.onupgradeneeded = () => {
+    //         db = request.result;
+    //         console.log('CREATE', db);
+    //         const objectStore = db.createObjectStore('userData', {
+    //             autoIncrement: true
+    //         });
+    //         console.log(db);
             
-        };
+    //     };
 
-        request.onerror = (error) => {
-            console.log('ERROR', error);
-        };
+    //     request.onerror = (error) => {
+    //         console.log('ERROR', error);
+    //     };
 
-        let data = { test: 'yes' };
+    //     // let data = { test: 'yes' };
 
-        addData = (data) => {
-            // let db;
-            // let request = indexedDB.open('userDB', 1);
-            // db = request.result;
-            console.log('ADD', db);
-            const transaction = db.transaction('userData', 'readwrite');
-            const objectStore = transaction.objectStore('userData');
-            request = objectStore.add(data);
-        }
+    //     addData = (data) => {
+    //         // let result
+    //         // readData();
+    //         // console.log(result)
 
-        readData = () => {
-            console.log('READ CURSOR');
-            const transaction = db.transaction('userData', 'readonly');
-            const objectStore = transaction.objectStore('userData');
-            request = objectStore.openCursor();
+    //         console.log('ADD', db);
+    //         const transaction = db.transaction('userData', 'readwrite');
+    //         const objectStore = transaction.objectStore('userData');
 
-            request.onsuccess = (e) => {
-                console.log(e.target);
-            }
-        }
+    //         let request = objectStore.openCursor();
+
+    //         request.onsuccess = (e) => {
+    //             console.log('RESULT', e.target.result.value);
+    //             console.log('RESULT', data);
+    //             let storedData = e.target.result.value;
+    //             if(JSON.stringify(storedData).split('').sort().join('') === JSON.stringify(data).split('').sort().join('')) {
+    //                 console.log('OBJECTS ARE THE SAME');
+    //                 return
+    //             }
+
+    //             let request1 = objectStore.add(data);
+    //             console.log('OBJECT WAS ADDED TO THE DATABASE');
+    //         }
+
+            
+    //     }
+
+    //     readData = () => {
+    //         console.log('READ CURSOR');
+
+    //         const transaction = db.transaction('userData', 'readonly');
+    //         const objectStore = transaction.objectStore('userData');
+    //         request = objectStore.openCursor();
+
+    //         request.onsuccess = (e) => {
+    //             console.log('RESULT', e.target);
+    //             // console.log('RESULT', e.target.result.value);
+    //         }
+    //     }
 
 
 
-        let url = dbUrl();
+    //     let url = dbUrl();
+
+    //     const queryString = window.location.search;
+    //     // console.log(queryString);
+    //     // const tempID = new URLSearchParams(window.location.search).get('tempID');
+    //     url += queryString
+
+    //     // console.log(tempID);
+    //     // shirt
+    //     // manageDB();
+    // }
+
+    let url = dbUrl();
 
         const queryString = window.location.search;
         // console.log(queryString);
@@ -74,17 +105,15 @@ async function afterLoad() {
             .then(data => {
 
                 console.log('data', data.userDB);
-                readData();
-                addData(data.userDB);
+                
+                // addData(data.userDB);
                 document.getElementById("beautifiedV").innerHTML = JSON.stringify(data.userDB.registroVacaciones, undefined, 2);
                 document.getElementById("beautifiedP").innerHTML = JSON.stringify(data.userDB.userPersonal, undefined, 2);
 
-                // document.getElementById("beautified").innerHTML = JSON.stringify(data.userDB, undefined, 2);
+                // return data.userDB;
             })
-            .then()
             .catch(error => console.log(error));
 
-    }
 }
 
 
