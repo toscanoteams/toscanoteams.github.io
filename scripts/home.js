fadfe0b23d74c8d8724cc927046c7d48613e4568
biotoscano <biotoscano@gmail.com>
2022-08-31 12:28:00 +0200

    user lateral pannel fixed

--- scripts/home.js
+++ scripts/home.js
@@ -1,13 +1,14 @@
+
+
 window.addEventListener('load', afterLoad);
 
-const indexedDB = window.indexedDB;
 
-let addData;
 
-let readData;
+// let addData;
+
+// let readData;
 
 async function afterLoad() {
-    console.log('function: manageDB()')
 
     // if (indexedDB) {
     //     let db;
@@ -26,7 +27,7 @@ async function afterLoad() {
     //             autoIncrement: true
     //         });
     //         console.log(db);
-            
+
     //     };
 
     //     request.onerror = (error) => {
@@ -59,7 +60,7 @@ async function afterLoad() {
     //             console.log('OBJECT WAS ADDED TO THE DATABASE');
     //         }
 
-            
+
     //     }
 
     //     readData = () => {
@@ -91,43 +92,27 @@ async function afterLoad() {
 
     let url = dbUrl();
 
-        const queryString = window.location.search;
-        // console.log(queryString);
-        // const tempID = new URLSearchParams(window.location.search).get('tempID');
-        url += queryString
+    const queryString = window.location.search;
+    // console.log(queryString);
+    // const tempID = new URLSearchParams(window.location.search).get('tempID');
+    url += queryString
 
-        // console.log(tempID);
-        // shirt
-        // manageDB();
+    // console.log(tempID);
 
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
 
-                console.log('data', data.userDB);
-                
-                // addData(data.userDB);
-                document.getElementById("beautifiedV").innerHTML = JSON.stringify(data.userDB.registroVacaciones, undefined, 2);
-                document.getElementById("beautifiedP").innerHTML = JSON.stringify(data.userDB.userPersonal, undefined, 2);
+            console.log('data', data.userDB);
 
-                // return data.userDB;
-            })
-            .catch(error => console.log(error));
+            // addData(data.userDB);
+
+            manageHeader(data.userDB.userPersonal)
+            
+            document.getElementById("beautifiedV").innerHTML = JSON.stringify(data.userDB.registroVacaciones, undefined, 2);
+
+
+        })
+        .catch(error => console.log(error));
 
 }
-
-
-
-
-
-
-
-// const addData = (data) => {
-//     let db;
-//     let request = indexedDB.open('userDB', 1);
-//     db = request.result;
-//     console.log(`db: ${db}`);
-//     const transaction = db.transaction('userData', 'readwrite');
-//     const objectStore = transaction.objectStore('userData');
-//     request = objectStore.add(data);
-// }
